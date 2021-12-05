f8a39028b137c4e8ee3997d01772e435a53c8bab
Adi Joost <adi.joost@gmail.com>
2021-12-05 13:53:37 +0100

    added requestes to requirements.txt

--- hakuna_call.py
+++ hakuna_call.py
@@ -1,2 +1,9 @@
 #Script to handle all API-Calls from Hakuna
+import os
+import requests
+
+#Gets User-token and Auth-Token for Hakuna from Enviroment-Variables
+user = os.environ.get("HAKUSER")
+personell_auth_token = os.environ.get("PERSHAKAUTH")
+organisation_auth_token = os.environ.get("ORGHAKAUTH") 
 
